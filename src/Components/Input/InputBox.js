ce76cd54ed06a32dc63496c5239079ab4176e1ad
leejiwon6315 <leejiwon6315@naver.com>
2021-01-11 21:06:51 +0900

    0111 create layout

---
+++ src/Components/Input/InputBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputBox = () => {
+  return <></>;
+};
+
+export default InputBox;
---
+++ src/Components/TimeLine/TimeLine.js
@@ -0,0 +1,229 @@
+import React from "react";
+import style from "./TimeLineStyle.module.scss";
+
+const TimeLine = () => {
+  return (
+    <div className={style.table_wrapper}>
+      <table className={style.table_header}>
+        <tbody>
+          <tr>
+            <th></th>
+            <td>월</td>
+            <td>화</td>
+            <td>수</td>
+            <td>목</td>
+            <td>금</td>
+            <td>토</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table className={style.table_body}>
+        <tbody>
+          <tr>
+            <th>
+              <div className={style.times}>
+                <div className={style.time}>오전 8시</div>
+                <div className={style.time}>오전 9시</div>
+                <div className={style.time}>오전 10시</div>
+                <div className={style.time}>오전 11시</div>
+                <div className={style.time}>오후 12시</div>
+                <div className={style.time}>오후 1시</div>
+                <div className={style.time}>오후 2시</div>
+                <div className={style.time}>오후 3시</div>
+                <div className={style.time}>오후 4시</div>
+                <div className={style.time}>오후 5시</div>
+                <div className={style.time}>오후 6시</div>
+                <div className={style.time}>오후 7시</div>
+                <div className={style.time}>오후 8시</div>
+              </div>
+            </th>
+
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TimeLine;
