fb15b513d46846fbf5496e20d0c3ac885e871e6f
leejiwon6315 <leejiwon6315@naver.com>
2021-01-12 22:50:48 +0900

    add modal

--- src/Components/Input/InputBox.js
+++ src/Components/Input/InputBox.js
@@ -1,70 +1,80 @@
 import React from "react";
 import style from "./InputBoxStyle.module.scss";
 
-const InputBox = () => {
+const InputBox = ({ modalState, closeModal }) => {
   return (
-    <div className={style.input_wrapper}>
-      <div className={style.input_body}>
-        <h3> 일정/수업</h3>
-        <input type="text"></input>
-        <h3>시작시간</h3>
-        <select name="time">
-          <option value="">08:00 - 08:30</option>
-          <option value="">08:30 - 09:00</option>
-          <option value="">09:00 - 09:30</option>
-          <option value="">09:30 - 10:00</option>
-          <option value="">10:00 - 10:30</option>
-          <option value="">10:30 - 11:00</option>
-          <option value="">11:00 - 11:30</option>
-          <option value="">11:30 - 12:00</option>
-          <option value="">12:00 - 12:30</option>
-          <option value="">12:30 - 13:00</option>
-          <option value="">13:00 - 13:30</option>
-          <option value="">13:30 - 14:00</option>
-          <option value="">14:00 - 14:30</option>
-          <option value="">14:30 - 15:00</option>
-          <option value="">15:00 - 15:30</option>
-          <option value="">15:30 - 16:00</option>
-          <option value="">16:00 - 16:30</option>
-          <option value="">16:30 - 17:00</option>
-          <option value="">17:00 - 17:30</option>
-          <option value="">17:30 - 18:00</option>
-          <option value="">18:00 - 18:30</option>
-          <option value="">18:30 - 19:00</option>
-          <option value="">19:00 - 19:30</option>
-          <option value="">19:30 - 20:00</option>
-        </select>
-        <h3>종료시간</h3>
-        <select name="time">
-          <option value="">08:00 - 08:30</option>
-          <option value="">08:30 - 09:00</option>
-          <option value="">09:00 - 09:30</option>
-          <option value="">09:30 - 10:00</option>
-          <option value="">10:00 - 10:30</option>
-          <option value="">10:30 - 11:00</option>
-          <option value="">11:00 - 11:30</option>
-          <option value="">11:30 - 12:00</option>
-          <option value="">12:00 - 12:30</option>
-          <option value="">12:30 - 13:00</option>
-          <option value="">13:00 - 13:30</option>
-          <option value="">13:30 - 14:00</option>
-          <option value="">14:00 - 14:30</option>
-          <option value="">14:30 - 15:00</option>
-          <option value="">15:00 - 15:30</option>
-          <option value="">15:30 - 16:00</option>
-          <option value="">16:00 - 16:30</option>
-          <option value="">16:30 - 17:00</option>
-          <option value="">17:00 - 17:30</option>
-          <option value="">17:30 - 18:00</option>
-          <option value="">18:00 - 18:30</option>
-          <option value="">18:30 - 19:00</option>
-          <option value="">19:00 - 19:30</option>
-          <option value="">19:30 - 20:00</option>
-        </select>
-        <h3>기타</h3>
-        <input type="text"></input>
-      </div>
-    </div>
+    <>
+      {modalState ? (
+        <>
+          <button className={style.close_button} onClick={closeModal}>
+            X
+          </button>
+
+          <div className={style.input_wrapper}>
+            <div className={style.input_body}>
+              <h3> 일정/수업</h3>
+              <input type="text"></input>
+              <h3>시작시간</h3>
+              <select name="time">
+                <option value="">08:00 - 08:30</option>
+                <option value="">08:30 - 09:00</option>
+                <option value="">09:00 - 09:30</option>
+                <option value="">09:30 - 10:00</option>
+                <option value="">10:00 - 10:30</option>
+                <option value="">10:30 - 11:00</option>
+                <option value="">11:00 - 11:30</option>
+                <option value="">11:30 - 12:00</option>
+                <option value="">12:00 - 12:30</option>
+                <option value="">12:30 - 13:00</option>
+                <option value="">13:00 - 13:30</option>
+                <option value="">13:30 - 14:00</option>
+                <option value="">14:00 - 14:30</option>
+                <option value="">14:30 - 15:00</option>
+                <option value="">15:00 - 15:30</option>
+                <option value="">15:30 - 16:00</option>
+                <option value="">16:00 - 16:30</option>
+                <option value="">16:30 - 17:00</option>
+                <option value="">17:00 - 17:30</option>
+                <option value="">17:30 - 18:00</option>
+                <option value="">18:00 - 18:30</option>
+                <option value="">18:30 - 19:00</option>
+                <option value="">19:00 - 19:30</option>
+                <option value="">19:30 - 20:00</option>
+              </select>
+              <h3>종료시간</h3>
+              <select name="time">
+                <option value="">08:00 - 08:30</option>
+                <option value="">08:30 - 09:00</option>
+                <option value="">09:00 - 09:30</option>
+                <option value="">09:30 - 10:00</option>
+                <option value="">10:00 - 10:30</option>
+                <option value="">10:30 - 11:00</option>
+                <option value="">11:00 - 11:30</option>
+                <option value="">11:30 - 12:00</option>
+                <option value="">12:00 - 12:30</option>
+                <option value="">12:30 - 13:00</option>
+                <option value="">13:00 - 13:30</option>
+                <option value="">13:30 - 14:00</option>
+                <option value="">14:00 - 14:30</option>
+                <option value="">14:30 - 15:00</option>
+                <option value="">15:00 - 15:30</option>
+                <option value="">15:30 - 16:00</option>
+                <option value="">16:00 - 16:30</option>
+                <option value="">16:30 - 17:00</option>
+                <option value="">17:00 - 17:30</option>
+                <option value="">17:30 - 18:00</option>
+                <option value="">18:00 - 18:30</option>
+                <option value="">18:30 - 19:00</option>
+                <option value="">19:00 - 19:30</option>
+                <option value="">19:30 - 20:00</option>
+              </select>
+              <h3>기타</h3>
+              <input type="text"></input>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 };
 
--- src/Components/Input/InputButton.js
+++ src/Components/Input/InputButton.js
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import InputBox from "./InputBox";
 
 const InputButton = () => {
-  const [modalOpen, setOpen] = useState(false);
+  const [modalState, setOpen] = useState(false);
 
+  const openModal = () => {
+    setOpen(true);
+    console.log(modalState);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+    console.log(modalState);
+  };
   return (
     <>
-      <button
-        onClick={() => {
-          !modalOpen ? setOpen(true) : setOpen(false);
-          console.log(modalOpen);
-        }}
-      >
-        시간표 추가
-      </button>
-
-      <InputBox />
+      <button onClick={openModal}>시간표 추가</button>
+      <InputBox modalState={modalState} closeModal={closeModal} />
     </>
   );
 };
