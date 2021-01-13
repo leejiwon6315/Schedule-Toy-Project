80537420baa6c669f258edceecacb9b544c3393b
leejiwon6315 <leejiwon6315@naver.com>
2021-01-13 22:44:00 +0900

    add table style

--- src/Components/TimeLine/TimeLine.js
+++ src/Components/TimeLine/TimeLine.js
@@ -8,12 +8,12 @@ const TimeLine = () => {
         <tbody>
           <tr>
             <th></th>
-            <td>월</td>
-            <td>화</td>
-            <td>수</td>
-            <td>목</td>
-            <td>금</td>
-            <td>토</td>
+            <td className={style.day}>월</td>
+            <td className={style.day}>화</td>
+            <td className={style.day}>수</td>
+            <td className={style.day}>목</td>
+            <td className={style.day}>금</td>
+            <td className={style.day}>토</td>
           </tr>
         </tbody>
       </table>
@@ -21,22 +21,20 @@ const TimeLine = () => {
       <table className={style.table_body}>
         <tbody>
           <tr>
-            <th>
-              <div className={style.times}>
-                <div className={style.time}>오전 8시</div>
-                <div className={style.time}>오전 9시</div>
-                <div className={style.time}>오전 10시</div>
-                <div className={style.time}>오전 11시</div>
-                <div className={style.time}>오후 12시</div>
-                <div className={style.time}>오후 1시</div>
-                <div className={style.time}>오후 2시</div>
-                <div className={style.time}>오후 3시</div>
-                <div className={style.time}>오후 4시</div>
-                <div className={style.time}>오후 5시</div>
-                <div className={style.time}>오후 6시</div>
-                <div className={style.time}>오후 7시</div>
-                <div className={style.time}>오후 8시</div>
-              </div>
+            <th className={style.times}>
+              <div className={style.time}>오전 8시</div>
+              <div className={style.time}>오전 9시</div>
+              <div className={style.time}>오전 10시</div>
+              <div className={style.time}>오전 11시</div>
+              <div className={style.time}>오후 12시</div>
+              <div className={style.time}>오후 1시</div>
+              <div className={style.time}>오후 2시</div>
+              <div className={style.time}>오후 3시</div>
+              <div className={style.time}>오후 4시</div>
+              <div className={style.time}>오후 5시</div>
+              <div className={style.time}>오후 6시</div>
+              <div className={style.time}>오후 7시</div>
+              <div className={style.time}>오후 8시</div>
             </th>
 
             <td>
@@ -66,34 +64,6 @@ const TimeLine = () => {
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
-                </div>
-              </div>
-            </td>
-            <td>
-              <div className={style.cols}>
-                <div className={style.grids}>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
                 </div>
@@ -104,34 +74,6 @@ const TimeLine = () => {
                 <div className={style.grids}>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                  <div className={style.grid}></div>
-                </div>
-              </div>
-            </td>
-            <td>
-              <div className={style.cols}>
-                <div className={style.grids}>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
@@ -186,12 +128,80 @@ const TimeLine = () => {
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
                 </div>
               </div>
             </td>
             <td>
               <div className={style.cols}>
                 <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className={style.cols}>
+                <div className={style.grids}>
+                  <div className={style.grid}></div>
+                  <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
                   <div className={style.grid}></div>
